9f036c5b63f8184772c54916363f9e28ec792104
Miroslav Pejic <miroslav.pejic@cloud.timenet.it>
2021-02-19 23:22:54 +0100

    [mirotalk] - map some  emoji from msger input txt

--- www/client.js
+++ www/client.js
@@ -17,6 +17,20 @@ const friendChatAvatar = "/images/friend.svg";
 const notifyBySound = true; // turn on-off sound notifications
 const isMobileDevice = DetectRTC.isMobileDevice;
 
+// https://github.com/wooorm/gemoji/blob/main/support.md
+var map = {
+  "<3": "\u2764\uFE0F",
+  "</3": "\uD83D\uDC94",
+  ":D": "\uD83D\uDE00",
+  ":)": "\uD83D\uDE03",
+  ";)": "\uD83D\uDE09",
+  ":(": "\uD83D\uDE12",
+  ":p": "\uD83D\uDE1B",
+  ";p": "\uD83D\uDE1C",
+  ":'(": "\uD83D\uDE22",
+  ":+1:": "\uD83D\uDC4D",
+}; //...
+
 var startTime;
 var elapsedTime;
 var mirotalkTheme = "neon"; // neon - dark - ghost ...
@@ -891,6 +905,14 @@ function setChatRoomBtn() {
     checkCountTime();
   });
 
+  // on input check 4emoji from map
+  get("msgerInput").oninput = function () {
+    for (var i in map) {
+      var regex = new RegExp(escapeSpecialChars(i), "gim");
+      this.value = this.value.replace(regex, map[i]);
+    }
+  };
+
   // chat send msg
   get("msgerSendBtn").addEventListener("click", (e) => {
     e.preventDefault(); // prevent refresh page
@@ -903,6 +925,11 @@ function setChatRoomBtn() {
 
     get("msgerInput").value = "";
   });
+}
+
+// Escape Special Chars
+function escapeSpecialChars(regex) {
+  return regex.replace(/([()[{*+.$^\\|?])/g, "\\$1");
 }
 
 // =====================================================
