0b092469d1ae5da9df072e50ea6c14c2d83a8915
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2021-05-20 20:05:32 +0200

    [mirotalk] - add mirotalk sky theme

--- www/js/client.js
+++ www/js/client.js
@@ -1071,6 +1071,26 @@ function setTheme(theme) {
         "3px 3px 6px #27944f, -3px -3px 6px #14843d"
       );
       break;
+    case "sky":
+      // sky theme
+      swalBackground = "rgba(0, 0, 0, 0.7)";
+      document.documentElement.style.setProperty("--body-bg", "black");
+      document.documentElement.style.setProperty("--msger-bg", "black");
+      document.documentElement.style.setProperty("--msger-private-bg", "black");
+      document.documentElement.style.setProperty("--left-msg-bg", "#03a5ce");
+      document.documentElement.style.setProperty("--private-msg-bg", "#f77070");
+      document.documentElement.style.setProperty("--right-msg-bg", "#012a5f");
+      document.documentElement.style.setProperty("--wb-bg", "#ffffff");
+      document.documentElement.style.setProperty("--wb-btn-color", "black");
+      document.documentElement.style.setProperty("--btn-bg", "white");
+      document.documentElement.style.setProperty("--btn-color", "black");
+      document.documentElement.style.setProperty("--btn-opc", "1");
+      document.documentElement.style.setProperty("--btns-left", "20px");
+      document.documentElement.style.setProperty(
+        "--box-shadow",
+        "3px 3px 6px #03a5ce, -3px -3px 6px #03a5ce"
+      );
+      break;
     case "ghost":
       // ghost theme
       swalBackground = "rgba(0, 0, 0, 0.150)";
