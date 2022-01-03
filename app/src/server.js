6b5e225b2bf099c19543718c3159c1558ab57e90
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-01-03 11:55:33 +0100

    [mirotalk] - fix typo

--- app/src/server.js
+++ app/src/server.js
@@ -20,7 +20,7 @@ dependencies: {
 }
 
 MiroTalk Signaling Server
-Copyright (C) 2021 Miroslav Pejic <miroslav.pejic.85@gmail.com>
+Copyright (C) 2022 Miroslav Pejic <miroslav.pejic.85@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
