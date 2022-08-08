ff566fbaab8618b83a55e8309c48430c003f98e6
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-08-08 20:49:05 +0200

    [mirotalk] - add colors log

--- app/src/Logger.js
+++ app/src/Logger.js
@@ -1,9 +1,45 @@
 'use strict';
 
+/**
+ * Log colors
+ */
+const Log = {
+    // mode
+    reset: '\x1b[0m',
+    bright: '\x1b[1m',
+    dim: '\x1b[2m',
+    underscore: '\x1b[4m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+    hidden: '\x1b[8m',
+    // Foreground (text) colors
+    fg: {
+        black: '\x1b[30m',
+        red: '\x1b[31m',
+        green: '\x1b[32m',
+        yellow: '\x1b[33m',
+        blue: '\x1b[34m',
+        magenta: '\x1b[35m',
+        cyan: '\x1b[36m',
+        white: '\x1b[37m',
+        crimson: '\x1b[38m',
+    },
+    // Background colors
+    bg: {
+        black: '\x1b[40m',
+        red: '\x1b[41m',
+        green: '\x1b[42m',
+        yellow: '\x1b[43m',
+        blue: '\x1b[44m',
+        magenta: '\x1b[45m',
+        cyan: '\x1b[46m',
+        white: '\x1b[47m',
+        crimson: '\x1b[48m',
+    },
+};
 module.exports = class Logger {
     constructor(appName, debugOn = true) {
-        if (appName) this.appName = appName;
-        else this.appName = 'mirotalk';
+        this.appName = Log.fg.yellow + appName + Log.reset;
         this.debugOn = debugOn;
     }
 
@@ -14,8 +50,7 @@ module.exports = class Logger {
      * @returns
      */
     debug(msg, op = '') {
-        if (this.debugOn === false) return;
-        console.debug('[' + this.getDataTime() + '] [' + this.appName + '] ' + msg, op);
+        if (this.debugOn) console.debug('[' + this.getDataTime() + '] [' + this.appName + '] ' + msg, op);
     }
 
     /**
@@ -63,6 +98,6 @@ module.exports = class Logger {
      * @returns {string} date to ISO string
      */
     getDataTime() {
-        return new Date().toISOString().replace(/T/, ' ').replace(/Z/, '');
+        return Log.fg.cyan + new Date().toISOString().replace(/T/, ' ').replace(/Z/, '') + Log.reset;
     }
 };
