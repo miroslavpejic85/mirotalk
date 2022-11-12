f8b7863eb0be4125adbe7af07e1edc99af823677
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-11-12 12:25:11 +0100

    [mirotalk] - fix typo

--- public/js/client.js
+++ public/js/client.js
@@ -12,7 +12,8 @@
  * @link    GitHub: https://github.com/miroslavpejic85/mirotalk
  * @link    Live demo: https://p2p.mirotalk.com or https://mirotalk.up.railway.app or https://mirotalk.herokuapp.com
  * @license For open source use: AGPLv3
- * @license For commercial or closed source, contact us at info.mirotalk@gmail.com
+ * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or buy directly via CodeCanyon
+ * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
  * @version 1.0.1
  *
