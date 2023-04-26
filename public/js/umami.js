f5f8bc3ad6c517967a876ba0ec3d9506b64954af
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-04-26 12:52:16 +0200

    [mirotalk] - improve umami

---
+++ public/js/umami.js
@@ -0,0 +1,7 @@
+// https://github.com/mikecao/umami
+
+const script = document.createElement('script');
+script.setAttribute('async', '');
+script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
+script.setAttribute('data-website-id', 'c7615aa7-ceec-464a-baba-54cb605d7261');
+document.head.appendChild(script);
