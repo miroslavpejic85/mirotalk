d9d67a39faa07955b5a82b7f69d0a492b4d25e32
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-05-29 13:35:51 +0200

    [mirotlak] - improve check XSS

--- app/src/xss.js
+++ app/src/xss.js
@@ -1,20 +1,54 @@
 'use strict';
 
 const xss = require('xss');
+const Logs = require('./logs');
+const log = new Logs('xss');
 
 /**
  * Prevent XSS injection by client side
- * @param {object} cfg
+ *
+ * @param {object} dataObject
  * @returns sanitized object
  */
-const checkXSS = (cfg) => {
-    if (typeof cfg === 'object' && Object.keys(cfg).length > 0) {
-        // Object to string and remove XSS injection
-        const config = xss(JSON.stringify(cfg));
-        // String sanitized to object
-        return JSON.parse(config);
+const checkXSS = (dataObject) => {
+    try {
+        if (typeof dataObject === 'object' && Object.keys(dataObject).length > 0) {
+            let objectJson = objectToJSONString(dataObject);
+            if (objectJson) {
+                let jsonString = xss(objectJson);
+                let jsonObject = JSONStringToObject(jsonString);
+                if (jsonObject) {
+                    log.debug('XSS Object sanitization done');
+                    return jsonObject;
+                }
+            }
+        }
+        if (typeof dataObject === 'string' || dataObject instanceof String) {
+            log.debug('XSS String sanitization done');
+            return xss(dataObject);
+        }
+        log.warn('XSS not sanitized', dataObject);
+        return dataObject;
+    } catch (error) {
+        log.error('XSS error', { data: dataObject, error: error });
+        return dataObject;
     }
-    return xss(cfg);
 };
+
+function objectToJSONString(dataObject) {
+    try {
+        return JSON.stringify(dataObject);
+    } catch (error) {
+        return false;
+    }
+}
+
+function JSONStringToObject(jsonString) {
+    try {
+        return JSON.parse(jsonString);
+    } catch (error) {
+        return false;
+    }
+}
 
 module.exports = checkXSS;
