f9f0303f965d1eac26efccfe3f8e2d08b984356f
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-07-26 15:03:04 +0200

    [mirotalk] - fix typo

--- app/api/meeting/meeting.js
+++ app/api/meeting/meeting.js
@@ -1,12 +1,14 @@
+'use strict';
+
 async function getMeeting() {
     try {
         // Use dynamic import with await
         const { default: fetch } = await import('node-fetch');
 
         const API_KEY = 'mirotalk_default_secret';
-        // $MIROTALK_URL = "http://localhost:3000/api/v1/meeting";
-        MIROTALK_URL = 'https://p2p.mirotalk.com/api/v1/meeting';
-        // $MIROTALK_URL = "https://mirotalk.up.railway.app/api/v1/meeting";
+        // const MIROTALK_URL = "http://localhost:3000/api/v1/meeting";
+        const MIROTALK_URL = 'https://p2p.mirotalk.com/api/v1/meeting';
+        // const MIROTALK_URL = "https://mirotalk.up.railway.app/api/v1/meeting";
 
         const response = await fetch(MIROTALK_URL, {
             method: 'POST',
