84cb31c2836392e2aa87cce1af06e3f95a48f403
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-02-12 13:16:00 +0100

    [mirotalk] - improve/update REST API

--- app/src/api.js
+++ app/src/api.js
@@ -5,7 +5,6 @@ const { v4: uuidV4 } = require('uuid');
 
 const JWT_KEY = process.env.JWT_KEY || 'mirotalk_jwt_secret';
 const JWT_EXP = process.env.JWT_EXP || '1h';
-
 module.exports = class ServerApi {
     constructor(host = null, authorization = null, api_key_secret = null) {
         this._host = host;
@@ -26,33 +25,48 @@ module.exports = class ServerApi {
         // Get data...
         const { room, name, audio, video, screen, hide, notify, token } = data;
 
+        const roomValue = room || uuidV4();
+        const nameValue = name || uuidV4();
+        const audioValue = audio || false;
+        const videoValue = video || false;
+        const screenValue = screen || false;
+        const hideValue = hide || false;
+        const notifyValue = notify || false;
+
         let jwtToken = '';
 
         if (token) {
+            // JWT.io
             const { username, password, presenter, expire } = token;
+
+            const usernameValue = username || 'username';
+            const passwordValue = password || 'password';
+            const presenterValue = String(presenter);
+            const expireValue = expire || JWT_EXP;
+
             jwtToken =
                 '&token=' +
-                jwt.sign({ username: username, password: password, presenter: presenter }, JWT_KEY, {
-                    expiresIn: expire ? expire : JWT_EXP,
+                jwt.sign({ username: usernameValue, password: passwordValue, presenter: presenterValue }, JWT_KEY, {
+                    expiresIn: expireValue,
                 });
         }
         return (
             this.getProtocol() +
             this._host +
             '/join?room=' +
-            room +
+            roomValue +
             '&name=' +
-            name +
+            nameValue +
             '&audio=' +
-            audio +
+            audioValue +
             '&video=' +
-            video +
+            videoValue +
             '&screen=' +
-            screen +
+            screenValue +
             '&hide=' +
-            hide +
+            hideValue +
             '&notify=' +
-            notify +
+            notifyValue +
             jwtToken
         );
     }
