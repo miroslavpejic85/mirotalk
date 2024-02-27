a28e7a666b5e08b8278e65e9bdca4dc2247be90a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-02-27 13:10:03 +0100

    [mirotalk] - add popup message if relay to TURN

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.2.91
+ * @version 1.2.92
  *
  */
 
@@ -1872,6 +1872,14 @@ async function handleOnIceCandidate(peer_id) {
                 candidate: event.candidate.candidate,
             },
         });
+        // Check if this is an ICE candidate for a relayed connection
+        if (event.candidate.candidate.indexOf('relay') !== -1) {
+            userLog(
+                'toast',
+                'WebRTC traffic is relayed through a TURN server due to restrictive NAT or firewall configurations',
+                6000,
+            );
+        }
     };
 }
 
