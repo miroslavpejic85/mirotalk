f248f06da6fda7542dd527922624b7b2161c9b7d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-02-28 21:32:54 +0100

    [mirotalk] - update dep

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.2.93
+ * @version 1.2.94
  *
  */
 
@@ -1920,11 +1920,6 @@ async function handleOnIceCandidate(peer_id) {
                 break;
             case 'relay':
                 networkTurn.innerText = '🟢';
-                userLog(
-                    'toast',
-                    'WebRTC traffic is relayed through a TURN server due to restrictive NAT or firewall configurations',
-                    6000,
-                );
                 break;
             default:
                 console.warn(`Unknown ICE candidate type: ${type}`);
