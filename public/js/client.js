e0edd71d3dd794ec53e989c67cd64ff4489a54bc
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-07-15 18:34:56 +0200

    [mirotalk] - fix typo, add header logo box-shadow

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.43
+ * @version 1.3.44
  *
  */
 
@@ -10065,7 +10065,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: '<strong>WebRTC P2P v1.3.43</strong>',
+        title: '<strong>WebRTC P2P v1.3.44</strong>',
         imageAlt: 'mirotalk-about',
         imageUrl: images.about,
         customClass: { image: 'img-about' },
