bc03eab0fc8089459797ffa579d7f8b3f8fdd891
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-07-22 17:18:12 +0200

    [mirotalk] - improve button hover

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.50
+ * @version 1.3.51
  *
  */
 
@@ -10114,7 +10114,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: '<strong>WebRTC P2P v1.3.50</strong>',
+        title: '<strong>WebRTC P2P v1.3.51</strong>',
         imageAlt: 'mirotalk-about',
         imageUrl: images.about,
         customClass: { image: 'img-about' },
