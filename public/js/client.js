18a395604a59f510e6c1fc1362a06c93bfbb1953
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-07-28 13:31:51 +0200

    [mirotalk] - improve emoji picker UI

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.57
+ * @version 1.3.58
  *
  */
 
@@ -10143,7 +10143,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: '<strong>WebRTC P2P v1.3.57</strong>',
+        title: '<strong>WebRTC P2P v1.3.58</strong>',
         imageAlt: 'mirotalk-about',
         imageUrl: images.about,
         customClass: { image: 'img-about' },
