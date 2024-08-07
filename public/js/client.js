959c5aba0efef19022a5b033451708ac3d2574d9
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-08-07 15:08:33 +0200

    [mirotalk] - rb 4 compatibility

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.63
+ * @version 1.3.64
  *
  */
 
@@ -10204,7 +10204,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: '<strong>WebRTC P2P v1.3.63</strong>',
+        title: '<strong>WebRTC P2P v1.3.64</strong>',
         imageAlt: 'mirotalk-about',
         imageUrl: images.about,
         customClass: { image: 'img-about' },
