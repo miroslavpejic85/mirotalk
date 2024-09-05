b0a7b090bc57d8d7b1d0c9741421da164d1d7a62
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-09-05 13:22:53 +0200

    [mirotalk] - change font, update dep

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.72
+ * @version 1.3.73
  *
  */
 
@@ -9266,7 +9266,7 @@ function whiteboardAddObj(type) {
             const text = new fabric.IText('Lorem Ipsum', {
                 top: 0,
                 left: 0,
-                fontFamily: 'Comfortaa',
+                fontFamily: 'Montserrat',
                 fill: wbCanvas.freeDrawingBrush.color,
                 strokeWidth: wbCanvas.freeDrawingBrush.width,
                 stroke: wbCanvas.freeDrawingBrush.color,
@@ -10527,7 +10527,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: '<strong>WebRTC P2P v1.3.72</strong>',
+        title: '<strong>WebRTC P2P v1.3.73</strong>',
         imageAlt: 'mirotalk-about',
         imageUrl: images.about,
         customClass: { image: 'img-about' },
