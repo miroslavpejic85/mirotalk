5e3af941c7cd96cf79afd3684dab7df8057dd2c0
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-04-06 14:42:45 +0200

    Revert "[mirotalk] - #280 add dark-yellow bg for private message"
    This reverts commit 8fe2f3a770bd22b48fb694879c7a7fb7e7c143af.

--- public/js/brand.js
+++ public/js/brand.js
@@ -73,7 +73,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.4.99',
+        title: 'WebRTC P2P v1.4.98',
         html: `
             <button 
                 id="support-button" 
