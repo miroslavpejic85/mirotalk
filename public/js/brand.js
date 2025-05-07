e01a7d9d686ad84c9da63710a404a7be86323cc2
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-05-08 01:10:01 +0200

    [mirotalk] - update dep

--- public/js/brand.js
+++ public/js/brand.js
@@ -73,7 +73,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.5.06',
+        title: 'WebRTC P2P v1.5.07',
         html: `
             <button 
                 id="support-button" 
