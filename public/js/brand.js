dd21de524274860a6a7e41099e46f451fe209bde
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-03 23:22:18 +0100

    [mirotalk] - improve chat UI

--- public/js/brand.js
+++ public/js/brand.js
@@ -75,7 +75,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.6.22',
+        title: 'WebRTC P2P v1.6.23',
         html: `
             <button 
                 id="support-button" 
