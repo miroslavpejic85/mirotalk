d1d554156fb4804d98044d6f435bf1f1855f65fb
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-11 15:07:18 +0100

    [mirotalk] - improve landing

--- public/js/brand.js
+++ public/js/brand.js
@@ -77,7 +77,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.6.31',
+        title: 'WebRTC P2P v1.6.32',
         html: `
             <button 
                 id="support-button" 
