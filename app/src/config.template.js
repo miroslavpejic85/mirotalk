99e05d6109aab2f9ebd0cd7a634b76a6fb2ea6dd
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-27 15:57:06 +0100

    [mirotalk] - responsive badge, update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.6.52 - Configuration File
+ * MiroTalk P2P v.1.6.53 - Configuration File
  * ==============================================
  *
  * Branding and customizations require a license:
