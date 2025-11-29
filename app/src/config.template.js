2f3aaccd807026119cf5e29a0c34d3561c7f6423
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-29 11:39:03 +0100

    [mirotalk] - add light effect

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.6.61 - Configuration File
+ * MiroTalk P2P v.1.6.62 - Configuration File
  * ==============================================
  *
  * Branding and customizations require a license:
--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.6.61
+ * @version 1.6.62
  *
  */
 
@@ -1162,7 +1162,7 @@ function countPeerConnections() {
  * Get Started...
  */
 document.addEventListener('DOMContentLoaded', function () {
-    //initCursorLightEffect();
+    initCursorLightEffect();
     initClientPeer();
 });
 
@@ -12648,7 +12648,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: brand.about?.title && brand.about.title.trim() !== '' ? brand.about.title : 'WebRTC P2P v1.6.61',
+        title: brand.about?.title && brand.about.title.trim() !== '' ? brand.about.title : 'WebRTC P2P v1.6.62',
         imageUrl: brand.about?.imageUrl && brand.about.imageUrl.trim() !== '' ? brand.about.imageUrl : images.about,
         customClass: { image: 'img-about' },
         html: `
