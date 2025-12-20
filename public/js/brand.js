9bb814dd819d26f2f7fea6abb9879997d461dc3e
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-20 16:48:40 +0100

    [mirotalk] - fix typo

--- public/js/brand.js
+++ public/js/brand.js
@@ -77,7 +77,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.6.91',
+        title: 'WebRTC P2P v1.6.92',
         html: `
             <button 
                 id="support-button" 
