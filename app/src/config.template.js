e837ecf3621f5129d8b9ea352dced0fcced0745a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-26 21:35:38 +0100

    [mirotalk] - improve whiteboard, update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.7.02 - Configuration File
+ * MiroTalk P2P v.1.7.03 - Configuration File
  * ==============================================
  *
  * Branding and customizations require a license:
