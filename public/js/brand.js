190eda217650d04abc7a25eb5d7b3a64e7396a74
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-01-18 00:59:48 +0100

    [mirotalk] - add missing

--- public/js/brand.js
+++ public/js/brand.js
@@ -77,7 +77,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.7.11',
+        title: 'WebRTC P2P v1.7.12',
         html: `
             <button 
                 id="support-button" 
--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.7.11
+ * @version 1.7.12
  *
  */
 
@@ -534,6 +534,7 @@ const pickr = Pickr.create({
     .on('changestop', () => {
         lsSettings.theme_color = themeCustom.color;
         lS.setSettings(lsSettings);
+        setTheme();
     });
 
 // Room
@@ -13563,7 +13564,7 @@ function showAbout() {
     Swal.fire({
         background: swBg,
         position: 'center',
-        title: brand.about?.title && brand.about.title.trim() !== '' ? brand.about.title : 'WebRTC P2P v1.7.11',
+        title: brand.about?.title && brand.about.title.trim() !== '' ? brand.about.title : 'WebRTC P2P v1.7.12',
         imageUrl: brand.about?.imageUrl && brand.about.imageUrl.trim() !== '' ? brand.about.imageUrl : images.about,
         customClass: { image: 'img-about' },
         html: `
