1917e41b4bee31b1cebdf208504ec2cf6638ada5
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-01-19 16:10:52 +0100

    [mirotalk] - update sponsor and dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.7.12 - Configuration File
+ * MiroTalk P2P v.1.7.13 - Configuration File
  * ==============================================
  *
  * Branding and customizations require a license:
