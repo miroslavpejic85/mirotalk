0b34f8d706c4632ba826a88d83e887bd7b9d3133
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-05-27 19:06:04 +0200

    [mirotalk] - chore(sponsors): add EvoPlat as sponsor

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.54 - Configuration File
+ * MiroTalk P2P v.1.8.55 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
