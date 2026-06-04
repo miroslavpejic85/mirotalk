05c575047a23163a8f96308bb2f09ae78ae3f2d8
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-06-04 17:35:35 +0200

    [mirotalk] - fix(coturn): fix TURN relay for VPS with IPv6 primary interface, update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.59 - Configuration File
+ * MiroTalk P2P v.1.8.60 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
