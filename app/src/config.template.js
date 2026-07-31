5861683909fe2b52a02bca3b8815ea4d704a0614
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-07-31 12:40:19 +0200

    [mirotalk] - update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.74 - Configuration File
+ * MiroTalk P2P v.1.8.75 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.8.74',
+        title: 'WebRTC P2P v1.8.75',
         html: `
             <button 
                 id="support-button" 
