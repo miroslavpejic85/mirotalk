b543f4e9219487cf1cca5833a6987cdcf3210441
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-15 20:20:10 +0200

    [mirotalk] - fix(client): align speaker select and test button on one line

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.99 - Configuration File
+ * MiroTalk P2P v.1.9.00 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
