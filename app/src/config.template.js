8b73a4fd7c08588f978c81c455155ca1ad8b73ea
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-18 18:45:50 +0200

    [mirotalk] - fix(room-emoji): render burst reliably by removing will-change and adding backdrop-filter fallback

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.03 - Configuration File
+ * MiroTalk P2P v.1.9.04 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -110,7 +110,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.03',
+        title: 'WebRTC P2P v1.9.04',
         html: `
             <button 
                 id="support-button" 
