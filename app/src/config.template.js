def070894e5f65009d3050b13c22e353995cc437
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-24 08:45:36 +0200

    [mirotalk] - style(client): refine leave button and add dropdown separators

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.12 - Configuration File
+ * MiroTalk P2P v.1.9.13 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -110,7 +110,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.12',
+        title: 'WebRTC P2P v1.9.13',
         html: `
             <button 
                 id="support-button" 
