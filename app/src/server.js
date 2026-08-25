133a53799caac45cc0a50789352d364aa46e95ca
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-25 12:30:47 +0200

    [mirotalk] - refactor: replace About page

--- app/src/server.js
+++ app/src/server.js
@@ -45,7 +45,7 @@ dependencies: {
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.9.15
+ * @version 1.9.16
  *
  */
 
@@ -366,7 +366,6 @@ const dir = {
 };
 // html views
 const views = {
-    about: path.join(__dirname, '../../', 'public/views/about.html'),
     client: path.join(__dirname, '../../', 'public/views/client.html'),
     landing: path.join(__dirname, '../../', 'public/views/landing.html'),
     login: path.join(__dirname, '../../', 'public/views/login.html'),
@@ -632,9 +631,9 @@ app.get('/stats', (req, res) => {
     res.send(statsData);
 });
 
-// mirotalk about
+// MiroTalk P2P documentation
 app.get(['/about'], (req, res) => {
-    res.sendFile(views.about);
+    res.redirect(301, 'https://docs.mirotalk.com/mirotalk-p2p/');
 });
 
 // privacy policy
