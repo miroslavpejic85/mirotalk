d6b28d771b7b52e7732ee106979e687fde615c2e
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-27 16:16:42 +0200

    [mirotalk] - style(ui): refine leave room dropdown menu

--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.20',
+        title: 'WebRTC P2P v1.9.21',
         html: `
             <button 
                 id="support-button" 
