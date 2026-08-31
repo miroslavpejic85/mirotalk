506cc05409b8942f48c23bd708ceb5d5624879bc
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-31 17:44:48 +0200

    [mirotalk] - feat(ui): improve camera and audio dropdown hierarchy

--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.27',
+        title: 'WebRTC P2P v1.9.28',
         html: `
             <button 
                 id="support-button" 
