8250c29a2cc38d59b1e856b8f0d7009408ee6d50
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-01 08:33:55 +0200

    [mirotalk] - feat(ui): Improve shared media exit button visibility

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.30 - Configuration File
+ * MiroTalk P2P v.1.9.31 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.30',
+        title: 'WebRTC P2P v1.9.31',
         html: `
             <button 
                 id="support-button" 
