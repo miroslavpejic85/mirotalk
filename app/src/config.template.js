a260648b540bbb03ab302431367305774294bbae
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-05 14:26:05 +0200

    [mirotalk] - fix(ui): improve mobile chat and settings responsiveness

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.58 - Configuration File
+ * MiroTalk P2P v.1.9.59 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
