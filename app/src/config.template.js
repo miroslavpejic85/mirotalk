690ac42e9f1482059d1c22e5f277c665a1a12bff
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-19 18:41:44 +0200

    [mirotalk] - update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.69 - Configuration File
+ * MiroTalk P2P v.1.9.70 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
