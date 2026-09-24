cf0ce3e28d7c6b4d7bd9ad22b7e01e816365fb8f
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-24 05:17:02 +0200

    [mirotalk] - fix(ui): reduce pinned video header button size

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.9.98 - Configuration File
+ * MiroTalk P2P v.1.9.99 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.9.98',
+        title: 'WebRTC P2P v1.9.99',
         html: `
             <button 
                 id="support-button" 
